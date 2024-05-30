f0315f87477b7119c7a8e5f720573ac8316606c7
MohameddAshraff89 <mohameddashraff69@gmail.com>
2024-05-30 11:43:18 +0200

    update

--- Backend/src/user-auth/register/register.controller.ts
+++ Backend/src/user-auth/register/register.controller.ts
@@ -1,19 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { RegisterService } from './register.service';
-import { EmailService } from '../email/email.service';
 import { User } from '../models/user.model';
 
 @Controller('register')
 export class RegisterController {
-  constructor(
-    private readonly registerService: RegisterService,
-    private readonly emailService: EmailService,
-  ) {}
+  constructor(private readonly registerService: RegisterService) {}
 
   @Post()
   async registerUser(@Body() userDetails: Partial<User>) {
     const newUser = await this.registerService.register(userDetails);
-    await this.emailService.sendVerificationEmail(newUser);
     return newUser; // Or whatever response you want to send back
   }
 }
