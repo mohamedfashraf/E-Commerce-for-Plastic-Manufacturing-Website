eaadd4ecec88109f417f8450b6b28eb29bdcc595
MohameddAshraff89 <mohameddashraff69@gmail.com>
2024-06-05 00:40:31 +0200

    removed unused

--- Backend/src/user-auth/register/register.controller.ts
+++ Backend/src/user-auth/register/register.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { RegisterResponse, RegisterService } from './register.service';
-import { EmailService } from '../email/email.service';
 import { User } from '../models/user.model';
 
 @Controller('register')
@@ -10,7 +9,6 @@ export class RegisterController {
   @Post()
   async registerUser(@Body() userDetails: Partial<User>):Promise<RegisterResponse>  {
     const {newUser,token} = await this.registerService.register(userDetails);
-    await this.emailService.sendVerificationEmail(newUser);
     return {newUser,token}; // Or whatever response you want to send back
   }
 }
